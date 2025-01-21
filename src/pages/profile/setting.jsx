aa1aaf318a1066984b2eb69f35c9f8e399cc669e
Sean Kim <seahnnnnnn@gmail.com>
2025-01-21 23:12:19 +0900

    feat: setting

--- src/pages/profile/setting.jsx
+++ src/pages/profile/setting.jsx
@@ -5,15 +5,30 @@ const Setting = ({ setIsLoggedIn }) => {
     const navigate = useNavigate();
 
     const handleLogout = () => {
-        setIsLoggedIn(false);
-        localStorage.removeItem('accessToken');
-        navigate('/login'); // 로그아웃 후 로그인 페이지로 이동
+        if (window.confirm('로그아웃 하시겠습니까?')) {
+            setIsLoggedIn(false);
+            localStorage.removeItem('accessToken');
+            navigate('/login'); // 로그아웃 후 로그인 페이지로 이동
+        }
     };
 
     return (
-        <div>
-            <h1>Setting</h1>
-            <button onClick={handleLogout}>Logout</button>
+        <div className="flex flex-col bg-white h-[calc(100vh-4rem)] w-full">
+            {/* 상단 헤더 */}
+            <div className="flex items-center justify-between px-4 py-3 border-b">
+                <button onClick={() => navigate(-1)}>
+                    <img src="/icons/chevron-left.svg" alt="뒤로가기" className="w-6 h-6" />
+                </button>
+                <h1 className="text-lg font-bold">
+                    환경설정
+                </h1>
+                <div className="w-6"></div>
+            </div>
+            {/* 로그아웃 버튼 */}
+            <div className="flex items-center justify-between px-4 py-3 border-b" onClick={handleLogout}>
+                <p className="text-md">로그아웃</p>
+                <img src="/icons/chevron-right.svg" alt="로그아웃" className="w-6 h-6" />
+            </div>
         </div>
     );
 };
